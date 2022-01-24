6c0470f6cda0bc94bfe20115d870716a62ed8507
felips95 <64986178+felips95@users.noreply.github.com>
2022-01-24 20:15:41 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ function menu() {
     let top = document.querySelector('.top');
     let menuIcon = document.querySelector('.menu-icon');
     let setaIcon = document.querySelector('.seta-icon');
-    let search = document.querySelector('.search');
 
     if (menu.style.display === 'none') {
 
-        menu.style.display = 'block';
-        search.style.display = 'none';        
+        menu.style.display = 'block';     
         body.style.overflow = 'hidden';
         top.style.position = 'fixed'; 
 
@@ -20,7 +18,6 @@ function menu() {
     else {
         menu.style.display = 'none';
         body.style.overflow = 'auto';        
-        search.style.display = 'flex';
         top.style.position = 'relative';
 
         menuIcon.style.display = 'block';
