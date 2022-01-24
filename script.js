2982fd69ce75fd9ec77992fda94f2547076ff8eb
felips95 <64986178+felips95@users.noreply.github.com>
2022-01-24 19:39:31 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 function menu() {
     let menu = document.querySelector('.menu');
     let body = document.querySelector('body');
-    let html = document.querySelector('html');
+    let header = document.querySelector('header');
     let menuIcon = document.querySelector('.menu-icon');
     let setaIcon = document.querySelector('.seta-icon');
     let search = document.querySelector('.search');
 
     if (menu.style.display === 'none') {
+
         menu.style.display = 'block';
-        setaIcon.style.display = 'block';
-        menuIcon.style.display = 'none';
         search.style.display = 'none';        
         body.style.overflow = 'hidden';
-        html.style.overflow = 'hidden';       
+        header.style.position = 'fixed'; 
+
+        setaIcon.style.display = 'block';
+        menuIcon.style.display = 'none';
+     
     } 
     else {
         menu.style.display = 'none';
-        body.style.overflow = 'auto';
-        html.style.overflow = 'auto';
-        menuIcon.style.display = 'block';
+        body.style.overflow = 'auto';        
         search.style.display = 'flex';
+        header.style.position = 'relative';
+
+        menuIcon.style.display = 'block';
         setaIcon.style.display = 'none'; 
     }
 }
